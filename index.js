942aaaef00e9ae5b7e7bd2fbb477781a8c0f7259
Jennifer Pazos <jmp521@gmail.com>
2022-04-18 17:16:18 -0500

    commit tree 29da827e21e8d3bf8dfcf9015035190aa7e5f17a

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((total, amount) => {
+    console.log("total =>", total)
+    console.log("amount =>", amount)
+    // return total + amount
+})
